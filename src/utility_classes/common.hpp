9122fc826968cfd9b9f6e59ebf8095a012f77b14
Matjaž Ostroveršnik <matjaz.ostroversnik@gmail.com>
2021-12-28 22:27:26 +0100

    log include added

--- src/utility_classes/common.hpp
+++ src/utility_classes/common.hpp
@@ -4,16 +4,19 @@
 #include <string>
 #include <string_view>
 #include <vector>
-namespace dbgen3 {
-///////////////////////////////////////////////////////////
-/// @brief common conts, functions and types
-///
 
-using cstr_t = std::string_view;
-using str_t = std::string;
-using str_vec = std::vector<std::string>;
+#include "log.hpp"
+namespace dbgen3
+{
+  ///////////////////////////////////////////////////////////
+  /// @brief common conts, functions and types
+  ///
 
-/**
+  using cstr_t  = std::string_view;
+  using str_t   = std::string;
+  using str_vec = std::vector<std::string>;
+
+  /**
  * @brief fetch token id
  *
  * The method returns the token id from the token set or 0
@@ -23,14 +26,14 @@ using str_vec = std::vector<std::string>;
  * @param token_set set of tokens that we are searching in
  * @return
  */
-int g_token_ndx(const str_t token, const str_vec &token_set);
-/**
+  int g_token_ndx(const str_t token, const str_vec& token_set);
+  /**
  * @brief it returns string associated with the token id from the token set.
  *
  * @param token_id token id that searching for string
  * @param token_set set of tokens
  * @return string version of the token id
  */
-const str_t g_token_str(int token_id, const str_vec &token_set);
+  const str_t g_token_str(int token_id, const str_vec& token_set);
 } // namespace dbgen3
 #endif // guard__COMMON_HPP_QSN7IE6JX4HO__
