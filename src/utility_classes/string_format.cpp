90f7ad1e60755646816ef234a831d64d7475b08a
Matjaž Ostroveršnik <matjaz.ostroversnik@gmail.com>
2022-01-08 21:59:12 +0100

    polishing

--- src/utility_classes/string_format.cpp
+++ src/utility_classes/string_format.cpp
@@ -26,26 +26,4 @@ namespace dbgen3
   {
     return s(a_prompt, a_msg, offs) + ln();
   }
-
-  // str_t string_format::s(cstr_t a_msg, uint8_t offs)
-  // {
-  //   return offs != 0
-  //            ? std::string(offs, ' ') + std::string(a_msg.data(), a_msg.size())
-  //            : std::string(a_msg.data(), a_msg.size());
-  // }
-
-  // str_t string_format::sl(cstr_t a_msg, uint8_t offs)
-  // {
-  //   return s(a_msg, offs) + ln();
-  // }
-
-  // str_t string_format::s(cstr_t a_prompt, cstr_t a_msg, uint8_t offs)
-  // {
-  //   return std::string(offs, ' ') + s(a_prompt) + s(a_msg);
-  // }
-
-  // str_t string_format::sl(cstr_t a_prompt, cstr_t a_msg, uint8_t offs)
-  // {
-  //   return s(a_prompt, a_msg, offs) + ln();
-  // }
 } // namespace dbgen3
